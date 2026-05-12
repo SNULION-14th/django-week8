279fa122e5cd3e845803ea7eac5ce49548c3876e
yerim <yerimryu@snu.ac.kr>
2026-05-13 08:05:26 +0900

    revision

--- seminar/settings.py
+++ seminar/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'django_extensions',
     'todo.apps.TodoConfig',
     'account.apps.AccountConfig',
