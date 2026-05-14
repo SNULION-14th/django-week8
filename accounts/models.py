3f7fc70b94d4f98e20d7a642584e251e66d097be
yanghyeonseo <yanghyeonseo1018@gmail.com>
2026-05-14 13:24:25 +0900

    fix: use AbstractUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 
 # Create your models here.
-class User(models.Model):
-  pw_hash = models.CharField(max_length=256)
-  email = models.EmailField()
-  name = models.CharField(max_length=128)
-  age = models.PositiveIntegerField()
-  job = models.CharField(max_length=128)
-  gender = models.CharField(max_length=128)
+class User(AbstractUser):
+  age = models.PositiveIntegerField(blank=True)
+  job = models.CharField(max_length=128, blank=True)
+  gender = models.CharField(max_length=128, blank=True)
   created_at = models.DateTimeField(default=timezone.now())
 
   def __str__(self):
