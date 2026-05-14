f39c30286cfb839de1b6d976eabe3be2f9335279
yanghyeonseo <yanghyeonseo1018@gmail.com>
2026-05-14 16:31:48 +0900

    fix: fix table relationships

--- accounts/models.py
+++ accounts/models.py
@@ -1,24 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
 class User(AbstractUser):
-  age = models.PositiveIntegerField(blank=True)
+  age = models.PositiveIntegerField(null=True)
   job = models.CharField(max_length=128, blank=True)
   gender = models.CharField(max_length=128, blank=True)
-  created_at = models.DateTimeField(default=timezone.now())
+  created_at = models.DateTimeField(default=timezone.now)
 
   def __str__(self):
-    return f'{self.name}({self.id})'
+    return f'{self.username}({self.id})'
 
 
 class Interest(models.Model):
-  user_id = models.ForeignKey("User", on_delete=models.CASCADE)
-  keyword = models.CharField(default=128)
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="interests", on_delete=models.CASCADE)
+  keyword = models.CharField(max_length=128)
   description = models.TextField()
   priority = models.IntegerField()
-  created_at = models.DateTimeField(default=timezone.now())
+  created_at = models.DateTimeField(default=timezone.now)
 
   def __str__(self):
     return f'{self.keyword}({self.id})'
--- notices/models.py
+++ notices/models.py
@@ -1,48 +1,54 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
 class Source(models.Model):
   name = models.CharField(max_length=128)
   url = models.URLField(max_length=1024)
   crawl_interval_minutes = models.PositiveIntegerField()
-  crawled_at = models.DateTimeField()
-  created_at = models.DateTimeField(default=timezone.now())
+  crawled_at = models.DateTimeField(null=True)
+  created_at = models.DateTimeField(default=timezone.now)
+
+  subscription_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='subscription_sources', through='SourceSubscription')
 
   def __str__(self):
     return f'{self.name}: {self.url} ({self.id})'
 
 
 class Notice(models.Model):
-  source_id = models.ForeignKey("Source", on_delete=models.CASCADE)
+  source = models.ForeignKey("Source", on_delete=models.CASCADE)
   url = models.URLField(max_length=1024)
-  hash = models.CharField(max_length=256)
+  content_hash = models.CharField(max_length=256)
   title = models.CharField(max_length=256)
   content = models.TextField()
   publisher = models.CharField(max_length=256)
   published_at = models.DateTimeField()
-  updated_at = models.DateTimeField()
-  created_at = models.DateTimeField(default=timezone.now())
+  updated_at = models.DateTimeField(null=True)
+  created_at = models.DateTimeField(default=timezone.now)
+
+  inbox_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='inbox_notices', through='InboxNotice')
 
   def __str__(self):
     return f'{self.title}({self.id})'
 
 
 class SourceSubscription(models.Model):
-  user_id = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
-  source_id = models.ForeignKey("Source", on_delete=models.CASCADE)
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  source = models.ForeignKey("Source", on_delete=models.CASCADE)
+  created_at = models.DateTimeField(default=timezone.now)
 
   def __str__(self):
-    return f'User {self.user_id} -> Source {self.source_id} ({self.id})'
+    return f'User {self.user} -> Source {self.source} ({self.id})'
 
 
 class InboxNotice(models.Model):
-  user_id = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
-  notice_id = models.ForeignKey("Notice", on_delete=models.CASCADE)
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  notice = models.ForeignKey("Notice", on_delete=models.CASCADE)
   relevance_score = models.FloatField()
   matched_keywords = models.TextField()
   reason = models.TextField()
-  is_read = models.BooleanField()
+  is_read = models.BooleanField(default=False)
 
   def __str__(self):
-    return f'User {self.user_id} -> Notice {self.notice_id} {self.id}'
+    return f'User {self.user} -> Notice {self.notice} {self.id}'
